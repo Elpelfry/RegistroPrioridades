6d7ce3e0615bd2df463d1c199ad5a52a9a911d1b
ElPelfry <delfry_paulino@ucne.edu.do>
2024-09-01 15:00:50 -0400

    MainActivity + DbInjection

--- app/src/main/java/edu/ucne/registroprioridades/MainActivity.kt
+++ app/src/main/java/edu/ucne/registroprioridades/MainActivity.kt
@@ -4,44 +4,57 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import edu.ucne.registroprioridades.data.local.database.PrioridadDb
 import edu.ucne.registroprioridades.ui.theme.RegistroPrioridadesTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var prioridadDb: PrioridadDb
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        prioridadDb = Room.databaseBuilder(
+            applicationContext,
+            PrioridadDb::class.java,
+            "Prioridad.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
         setContent {
             RegistroPrioridadesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        PrioridadScreen()
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun PrioridadScreen() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RegistroPrioridadesTheme {
-        Greeting("Android")
+    }
+
+
+    @Preview(showBackground = true, showSystemUi = true)
+    @Composable
+    fun GreetingPreview() {
+        RegistroPrioridadesTheme {
+            PrioridadScreen()
+        }
     }
 }
