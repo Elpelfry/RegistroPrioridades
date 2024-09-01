02de65c74d07594b3f9e0ba0fefe81ed3be9e7f1
ElPelfry <delfry_paulino@ucne.edu.do>
2024-09-01 15:04:03 -0400

    Flow list table

--- app/src/main/java/edu/ucne/registroprioridades/MainActivity.kt
+++ app/src/main/java/edu/ucne/registroprioridades/MainActivity.kt
@@ -5,15 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
@@ -23,8 +31,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -65,6 +75,74 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun PrioridadScreen() {
 
+    }
+
+    @Composable
+    fun PrioridadListScreen(prioridadList: List<PrioridadEntity>) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                fontWeight = FontWeight.Bold,
+                text = "Lista de Prioridades",
+                style = MaterialTheme.typography.bodyLarge,
+
+                )
+            Spacer(modifier = Modifier.height(12.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp, vertical = 4.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier.weight(1f),
+                    fontWeight = FontWeight.Bold,
+                    text = "Descripción",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    fontWeight = FontWeight.Bold,
+                    text = "Días Compromiso",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
+            }
+            HorizontalDivider()
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(prioridadList) { prioridad ->
+                    PrioridadRow(prioridad)
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun PrioridadRow(prioridad: PrioridadEntity) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 14.dp, vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier.weight(4f),
+                text = prioridad.descripcion,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                modifier = Modifier.weight(1f),
+                text = prioridad.diasCompromiso?.toString() ?: "N/A",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        HorizontalDivider()
     }
 
     private suspend fun savePrioridad(prioridad: PrioridadEntity) {
