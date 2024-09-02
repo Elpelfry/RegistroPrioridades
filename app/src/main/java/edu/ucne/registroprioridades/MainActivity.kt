0acc321f297a35475a26b510cdd83032af05a2dd
ElPelfry <delfry_paulino@ucne.edu.do>
2024-09-02 13:00:51 -0400

    PrioridadScreen ajustes y reduccion de codigo

--- app/src/main/java/edu/ucne/registroprioridades/MainActivity.kt
+++ app/src/main/java/edu/ucne/registroprioridades/MainActivity.kt
@@ -186,6 +186,7 @@ class MainActivity : ComponentActivity() {
                                     )
                                     descripcion = ""
                                     diasCompromiso = ""
+                                    errorMessage = ""
                                 }
                             }
                         }
