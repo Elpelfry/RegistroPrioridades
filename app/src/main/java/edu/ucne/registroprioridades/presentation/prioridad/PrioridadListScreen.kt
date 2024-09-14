bbec0316e97f420aad53971788e9dffa1cd3baf4
ElPelfry <delfry_paulino@ucne.edu.do>
2024-09-14 18:09:45 -0400

    PrioridadScreen y PrioridadListScreen

--- app/src/main/java/edu/ucne/registroprioridades/presentation/prioridad/PrioridadListScreen.kt
+++ app/src/main/java/edu/ucne/registroprioridades/presentation/prioridad/PrioridadListScreen.kt
@@ -1,8 +1,11 @@
 package edu.ucne.registroprioridades.presentation.prioridad
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -19,73 +21,65 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import edu.ucne.registroprioridades.data.local.entities.PrioridadEntity
+import edu.ucne.registroprioridades.presentation.components.TopBarComponent
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
 
 @Composable
 fun PrioridadListScreen(
-    viewModel: PrioridadViewModel,
-    prioridadList: List<PrioridadEntity>,
-    onEdit: (PrioridadEntity) -> Unit,
+    viewModel: PrioridadViewModel = hiltViewModel(),
+    onEdit: (Int) -> Unit,
     onAddPrioridad: () -> Unit,
-    onDeletePrioridad:(PrioridadEntity) -> Unit
+    onDrawer: () -> Unit
 )  {
-    val prioridades by viewModel.prioridad.collectAsStateWithLifecycle()
-
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     PrioridadListBody(
-        prioridadList = prioridadList,
+        uiState = uiState,
+        onEvent = viewModel::onEvent,
         onEdit = onEdit,
         onAddPrioridad = onAddPrioridad,
-        onDeletePrioridad = onDeletePrioridad
+        onDrawer = onDrawer
     )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PrioridadListBody(
-    prioridadList: List<PrioridadEntity>,
-    onEdit: (PrioridadEntity) -> Unit,
+    uiState: UiState,
+    onEvent: (PrioridadUiState) -> Unit,
+    onEdit: (Int) -> Unit,
     onAddPrioridad: () -> Unit,
-    onDeletePrioridad:(PrioridadEntity) -> Unit
+    onDrawer: () -> Unit
 ) {
     Scaffold (
         modifier = Modifier.fillMaxSize(),
         topBar = {
-            TopAppBar(
-                title = {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        Text(
-                            text = "Prioridades",
-                            style = MaterialTheme.typography.headlineLarge,
-                            fontWeight = FontWeight.Bold,
-                            color = Color(0xFF0275d8)
-                        )
-                    }
-
-                }
+            TopBarComponent(
+                title = "Prioridades",
+                onMenuClick = { onDrawer() }
             )
         },
         floatingActionButton = {
@@ -99,73 +93,106 @@ fun PrioridadListBody(
         }
     ){
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
                 .padding(it)
         ) {
             LazyColumn(
                 modifier = Modifier.fillMaxSize()
             ) {
-                items(prioridadList) { prioridad ->
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
-                            .clickable {
-                                prioridad.prioridadId?.let { it1 -> onEdit(prioridad) }
-                            }
-                            .border(0.5.dp, Color(0xFF0275d8), RoundedCornerShape(8.dp)),
-                        elevation = CardDefaults.cardElevation(8.dp),
-                        colors = CardDefaults.cardColors(
-                            Color(0xFFf4eeec),
-                        )
-                    ) {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(25.dp),
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ) {
-                            Column(
-                                modifier = Modifier.weight(4f),
-                            ) {
-                                Text(
-                                    text = prioridad.descripcion,
-                                    style = MaterialTheme.typography.bodyLarge,
-                                    fontWeight = FontWeight.Bold,
-                                    color = Color(0xFF0275d8)
-                                )
-                                Spacer(modifier = Modifier.height(4.dp))
-                                Text(
-                                    text = "Días Compromiso: ${prioridad.diasCompromiso?.toString() ?: "N/A"}",
-                                    style = MaterialTheme.typography.bodySmall,
-                                    color = Color.DarkGray,
-                                    fontWeight = FontWeight.Bold,
-                                )
-                            }
-                            IconButton(
-                                onClick = {
-                                    prioridad.prioridadId?.let { it1 -> onDeletePrioridad(prioridad) }
+                items(uiState.prioridades) { prioridad ->
+
+                    val coroutineScope = rememberCoroutineScope()
+                    val dismissState = rememberSwipeToDismissBoxState(
+                        confirmValueChange = { state ->
+                            if (state == SwipeToDismissBoxValue.EndToStart) {
+                                coroutineScope.launch {
+                                    delay(0.5.seconds)
+                                    onEvent(PrioridadUiState.Delete(prioridad.prioridadId))
                                 }
+                                true
+                            } else {
+                                false
+                            }
+                        }
+                    )
+                    SwipeToDismissBox(
+                        state = dismissState,
+                        enableDismissFromStartToEnd = false,
+                        backgroundContent = {
+                            val color by animateColorAsState(
+                                when (dismissState.targetValue) {
+                                    SwipeToDismissBoxValue.Settled -> Color.White
+                                    SwipeToDismissBoxValue.EndToStart -> Color.Red
+                                    SwipeToDismissBoxValue.StartToEnd -> TODO()
+                                }, label = "Changing color"
+                            )
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(color, shape = RoundedCornerShape(8.dp))
+                                    .padding(16.dp),
+                                contentAlignment = Alignment.CenterEnd
                             ) {
                                 Icon(
                                     imageVector = Icons.Default.Delete,
-                                    contentDescription = "Eliminar prioridad",
-                                    tint = Color(0xFFb42f2f)
+                                    contentDescription = null,
+                                    tint = Color.White
                                 )
                             }
+
+                        },
+                        modifier = Modifier
+                    ) {
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                                .clickable {
+                                    prioridad.prioridadId?.let { it1 -> onEdit(prioridad.prioridadId) }
+                                }
+                                .border(0.5.dp, Color(0xFF0275d8), RoundedCornerShape(8.dp)),
+                            elevation = CardDefaults.cardElevation(8.dp),
+                            colors = CardDefaults.cardColors(
+                                Color(0xFFf4eeec),
+                            )
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(25.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Column(
+                                    modifier = Modifier.weight(4f),
+                                ) {
+                                    Text(
+                                        text = prioridad.descripcion,
+                                        style = MaterialTheme.typography.bodyLarge,
+                                        fontWeight = FontWeight.Bold,
+                                        color = Color(0xFF0275d8)
+                                    )
+                                    Spacer(modifier = Modifier.height(4.dp))
+                                    Text(
+                                        text = "Días Compromiso: ${prioridad.diasCompromiso?.toString() ?: "N/A"}",
+                                        style = MaterialTheme.typography.bodySmall,
+                                        color = Color.DarkGray,
+                                        fontWeight = FontWeight.Bold,
+                                    )
+                                }
+                            }
+                            HorizontalDivider(
+                                modifier = Modifier.padding(horizontal = 20.dp),
+                                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f)
+                            )
                         }
-                        HorizontalDivider(
-                            modifier = Modifier.padding(horizontal = 20.dp),
-                            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f)
-                        )
                     }
                 }
             }
         }
     }
 }
-
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -189,9 +216,10 @@ private fun PrioridadListPreview() {
         )
     )
     PrioridadListBody(
-        prioridadList = prioridades,
+        uiState = UiState(prioridades = prioridades),
+        onEvent = {},
         onEdit = {},
         onAddPrioridad = {},
-        onDeletePrioridad = {}
+        onDrawer = {}
     )
 }
