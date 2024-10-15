4b11fa3dd189179543ffd86a5a1d6bfb39591a14
ElPelfry <delfry_paulino@ucne.edu.do>
2024-10-15 17:03:45 -0400

    Sistema y Product Database

--- app/src/main/java/edu/ucne/registroprioridades/data/local/database/TicketDb.kt
+++ app/src/main/java/edu/ucne/registroprioridades/data/local/database/TicketDb.kt
@@ -3,21 +3,29 @@ package edu.ucne.registroprioridades.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.registroprioridades.data.local.dao.PrioridadDao
+import edu.ucne.registroprioridades.data.local.dao.ProductDao
+import edu.ucne.registroprioridades.data.local.dao.SistemaDao
 import edu.ucne.registroprioridades.data.local.dao.TicketDao
 import edu.ucne.registroprioridades.data.local.entities.PrioridadEntity
+import edu.ucne.registroprioridades.data.local.entities.ProductEntity
+import edu.ucne.registroprioridades.data.local.entities.SistemaEntity
 import edu.ucne.registroprioridades.data.local.entities.TicketEntity
 
 @Database(
 
     entities = [
         PrioridadEntity::class,
-        TicketEntity::class
+        TicketEntity::class,
+        SistemaEntity::class,
+        ProductEntity::class
     ],
-    version = 2,
+    version = 5,
     exportSchema = false
 )
 
 abstract class TicketDb : RoomDatabase(){
     abstract fun prioridadDao(): PrioridadDao
     abstract fun ticketDao(): TicketDao
+    abstract fun sistemaDao(): SistemaDao
+    abstract fun productDao(): ProductDao
 }
