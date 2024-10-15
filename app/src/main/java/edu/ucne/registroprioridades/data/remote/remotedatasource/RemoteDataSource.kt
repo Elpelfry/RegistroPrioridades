5cbe11ddf6e94a1e29491d9345b2f40954463f08
ElPelfry <delfry_paulino@ucne.edu.do>
2024-10-15 17:04:31 -0400

    Se agrega RemoteDataSource

--- app/src/main/java/edu/ucne/registroprioridades/data/remote/remotedatasource/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/registroprioridades/data/remote/remotedatasource/RemoteDataSource.kt
@@ -1,14 +1,22 @@
 package edu.ucne.registroprioridades.data.remote.remotedatasource
 
 import edu.ucne.registroprioridades.data.remote.api.TicketApi
+import edu.ucne.registroprioridades.data.remote.dto.ProductDto
 import edu.ucne.registroprioridades.data.remote.dto.SistemaDto
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
     private val ticketApi: TicketApi
 ) {
+    // Sistemas
     suspend fun getSistemas() = ticketApi.getSistemas()
     suspend fun addSistemas(sistemaDto: SistemaDto) = ticketApi.addSistemas(sistemaDto)
     suspend fun updateSistemas(id: Int, sistemaDto: SistemaDto) = ticketApi.updateSistemas(id, sistemaDto)
     suspend fun deleteSistemas(id: Int) = ticketApi.deleteSistemas(id)
+
+    // Products
+    suspend fun getProducts() = ticketApi.getProducts()
+    suspend fun addProduct(productDto: ProductDto) = ticketApi.addProduct(productDto)
+    suspend fun updateProduct(id: Int, productDto: ProductDto) = ticketApi.updateProduct(id, productDto)
+    suspend fun deleteProduct(id: Int) = ticketApi.deleteProduct(id)
 }
