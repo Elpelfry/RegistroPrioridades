7291bbbf91a95935910f5d0b5cf4ffee44989bc2
ElPelfry <delfry_paulino@ucne.edu.do>
2024-10-15 17:06:45 -0400

    Se agrega repository para Product y nuevo Repository para Sistema

--- app/src/main/java/edu/ucne/registroprioridades/data/repository/SistemaRepository.kt
+++ app/src/main/java/edu/ucne/registroprioridades/data/repository/SistemaRepository.kt
@@ -1,15 +1,100 @@
 package edu.ucne.registroprioridades.data.repository
 
-import edu.ucne.registroprioridades.data.remote.api.TicketApi
+import edu.ucne.registroprioridades.data.local.dao.SistemaDao
+import edu.ucne.registroprioridades.data.local.entities.SistemaEntity
 import edu.ucne.registroprioridades.data.remote.dto.SistemaDto
+import edu.ucne.registroprioridades.data.remote.dto.toEntity
 import edu.ucne.registroprioridades.data.remote.remotedatasource.RemoteDataSource
+import edu.ucne.registroprioridades.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class SistemaRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val sistemaDao: SistemaDao
 ) {
-    suspend fun getSistemas(): List<SistemaDto> = remoteDataSource.getSistemas()
-    suspend fun addSistemas(sistemaDto: SistemaDto) = remoteDataSource.addSistemas(sistemaDto)
-    suspend fun updateSistemas(id: Int, sistemaDto: SistemaDto) = remoteDataSource.updateSistemas(id, sistemaDto)
-    suspend fun deleteSistemas(id: Int) = remoteDataSource.deleteSistemas(id)
+    fun getSistemas(): Flow<Resource<List<SistemaEntity>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val sistemasR = remoteDataSource.getSistemas()
+
+            val sistemasL = sistemasR.map { it.toEntity() }
+            sistemasL.forEach { sistemaDao.save(it) }
+
+            emit(Resource.Success(sistemasL))
+        } catch (e: HttpException) {
+            emit(Resource.Error("Error de conexión ${e.message()}"))
+        } catch (e: Exception) {
+
+            val sistemasL = sistemaDao.getAll().firstOrNull()
+            if (sistemasL.isNullOrEmpty()) {
+                emit(Resource.Error("No se encontraron datos locales"))
+            } else {
+                emit(Resource.Success(sistemasL))
+            }
+        }
+    }
+
+    fun addSistemas(sistemaDto: SistemaDto): Flow<Resource<SistemaEntity>> = flow {
+        try {
+            emit(Resource.Loading())
+            val sistemaR = remoteDataSource.addSistemas(sistemaDto)
+            val sistemaL = sistemaR.toEntity()
+            sistemaDao.save(sistemaL)
+            emit(Resource.Success(sistemaL))
+        } catch (e: HttpException) {
+            emit(Resource.Error("Error de conexión ${e.message()}"))
+        } catch (e: Exception) {
+            emit(Resource.Error("Error ${e.message}"))
+        }
+    }
+
+
+    fun updateSistemas(id: Int, sistemaDto: SistemaDto): Flow<Resource<SistemaEntity>> = flow {
+        try {
+            emit(Resource.Loading())
+            val sistemaR = remoteDataSource.updateSistemas(id, sistemaDto)
+            val sistemaL = sistemaR.toEntity()
+            sistemaDao.save(sistemaL)
+            emit(Resource.Success(sistemaL))
+        } catch (e: HttpException) {
+            emit(Resource.Error("Error de conexión ${e.message()}"))
+        } catch (e: Exception) {
+            emit(Resource.Error("Error ${e.message}"))
+        }
+    }
+
+    suspend fun deleteSistemas(id: Int): Resource<Unit> {
+        return try {
+            remoteDataSource.deleteSistemas(id)
+            val sistemaL = sistemaDao.find(id)
+            if (sistemaL != null) {
+                sistemaDao.delete(sistemaL)
+            }
+            Resource.Success(Unit)
+        } catch (e: HttpException) {
+            Resource.Error("Error de conexión ${e.message()}")
+        } catch (e: Exception) {
+            val sistemaL = sistemaDao.find(id)
+            if (sistemaL != null) {
+                sistemaDao.delete(sistemaL)
+            }
+            Resource.Success(Unit)
+        }
+    }
+
+    fun find(id: Int): Flow<Resource<SistemaEntity>> = flow {
+        try {
+            emit(Resource.Loading())
+            val sistema = sistemaDao.find(id)
+            emit(Resource.Success(sistema!!))
+        } catch (e: HttpException) {
+            emit(Resource.Error("Error de conexión ${e.message()}"))
+        } catch (e: Exception) {
+            emit(Resource.Error("Error ${e.message}"))
+        }
+    }
 }
